814f27f749f6eaa661faf43579312a81911d418f
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2020-11-08 21:27:50 +0100

    Clean

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,16 +7,6 @@ interface HeadingProps {
   className?: string;
 }
 
-const elements = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  h5: 'h5',
-  h6: 'h6',
-};
-
-const Heading: React.FC<HeadingProps> = ({ children, type, ...props }) =>
-  React.createElement(elements[type] || elements.h1, props, children);
+const Heading: React.FC<HeadingProps> = ({ children, type, ...props }) => React.createElement(type, props, children);
 
 export default Heading;
