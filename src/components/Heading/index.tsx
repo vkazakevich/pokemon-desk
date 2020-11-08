cf3341fe7eb714b872161ecdd99f262696cc6163
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2020-11-08 21:39:36 +0100

    Clean

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,7 +4,6 @@ type Types = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 interface HeadingProps {
   type: Types;
-  className?: string;
 }
 
 const Heading: React.FC<HeadingProps> = ({ children, type, ...props }) => React.createElement(type, props, children);
